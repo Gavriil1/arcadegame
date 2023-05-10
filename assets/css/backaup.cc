62716e2e09ccd1f87ce6399cb24051d62add7f62
Spyropoulos <gavriil.spyropoulos@dell.com>
2023-05-10 19:42:24 +0100

    Adding EmailJS API to the website for the form submission

---
+++ assets/css/backaup.cc
@@ -0,0 +1,76 @@
+/*
+@media only screen and (max-width: 1500px) {
+	h1 {
+		font-size: 60px ;
+	}
+	/* HTML canvas CSS */
+	canvas {
+		border: 1px solid black;
+		width: 90%;   
+		height: 35%;
+	}
+	/* Left Right Fire buttons' css */
+	button {
+		width: 29%;
+		height: 100%;
+		font-size: 200%;
+		margin-right: 5px;
+	}
+	/* footer css*/
+	.footer {
+		background-color: red;
+		width: 88%;
+		margin: auto;
+		display: inline-block;
+		/* text-align: center; */
+		font-size: 200%;
+	}
+  }
+
+  @media only screen and (max-width: 580px){
+	h1 {
+		font-size: 60px ;
+	}
+	/* HTML canvas CSS */
+	canvas {
+		border: 1px solid black;
+		width: 90%;   
+		height: 35%;
+	}
+	/* Left Right Fire buttons' css */
+	button {
+		width: 30%;
+		height: 100%;
+		font-size: font-size: 200%;;
+
+	}
+	/* footer css*/
+	.footer {
+		background-color: red;
+		width: 90%;
+		margin: auto;
+		/* text-align: center; */
+		font-size: 150%;
+	}
+
+  }
+
+/* Code of 404 page */
+.bodyfof {
+	background-color: rgb(208, 238, 196);
+}
+  
+.hfof{
+	font-size: 40px ; 
+	/* border-style: dotted */
+	margin-top: 0px;
+  	margin-bottom: 0px;
+	padding-bottom:0px;
+}
+
+.pfof {
+	margin-top: 0px;
+	margin-bottom: 5px;
+	padding-bottom: 0px;
+	/* border-style: dotted */
+}
