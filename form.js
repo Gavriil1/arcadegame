cf24bf24801b91ebaa9f1a41d6167dc3ebc0f073
Spyropoulos <gavriil.spyropoulos@dell.com>
2023-05-11 12:37:01 +0100

    start work on readme file again

---
+++ form.js
@@ -0,0 +1,20 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('ZeVrVpnrISRsAcECy');
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+                document.location.href="/";
+                window.alert("Your Feedback was received. Thank you")
+            }, function(error) {
+                console.log('FAILED...', error);
+                window.alert( "Unfortunately, your feedback was not submitted successfully. Please try again later.")
+            });
+    });
+}
