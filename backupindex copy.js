33f373e3ab8e058be5005a65958381843309b9d1
Spyropoulos <gavriil.spyropoulos@dell.com>
2023-05-11 17:55:24 +0100

    added comments to indxex.js file

--- backupindex copy.js
+++ backupindex copy.js
@@ -4,15 +4,20 @@ const kanhtml = document.querySelector('canvas');
 const d = kanhtml.getContext('2d');
  kanhtml.width = 1024;
  kanhtml.height = 576;
-// Archer Script. With this script  we defind properties of an archer. on a screen and his movement.
+
+
+// payer script
 class Archer {
     constructor(){
-        // Motion of the character which he has be default
+       
+
         this.celerity = {
             x:0,
             y:0
         }
-        //Properties of archer: picture, width, height, initial placement on the screen
+
+  
+
         const picture = new Image()
         picture.src = "assets/images/cartoon-martial-arts-character-archery_4042629.png"
         picture.onload = () => {
@@ -26,11 +31,13 @@ class Archer {
             }
         }
     }
-    //this function draws character on html canval, with the properties: picture, location(x,y) width and height
     draw(){
+        // d.fillStyle = 'red'
+        // d.fillRect(this.placement.x, this.placement.y, this.width, this.height)
+        
         d.drawImage(this.picture, this.placement.x, this.placement.y, this.width, this.height)
     }
-    //this function moves the character when user presses keys
+
     update() {
         if (this.picture){
          this.draw()
@@ -38,15 +45,14 @@ class Archer {
         }
     }
 }
-// Arrow script
+// projectie script
 class Arrow {
-    //Initial properties of arrows
     constructor({placement, celerity}){
         this.placement = placement
         this.celerity = celerity
         this.radius = 7
     }
-    //Function draw arrow on the screen
+
     draw() {
         d.beginPath()
         d.arc(this.placement.x, this.placement.y, this.radius, 0, Math.PI * 2)
@@ -54,23 +60,23 @@ class Arrow {
         d.fill()
         d.closePath()
     }
-    // update function defines movements of the arrows on the screen
+
     update() {
         this.draw()
         this.placement.x += this.celerity.x
         this.placement.y += this.celerity.y
     }
+
 }
-// Class which defines spartan warriors 
+// invaders
 class Spartan {
-    //motion of Spartans
     constructor(x, y) {
       this.celerity = {
         x: 3,
         y: 0
       };
       
-      // Defining properties of Spartans: picture, width, gheight, initial placement etc
+  
       const picture = new Image();
       picture.src = "assets/images/ancient-greek-spartan-warrior-6839912.png";
       picture.onload = () => {
@@ -84,30 +90,34 @@ class Spartan {
         };
       };
     }
-    //function draws spartans on the map
+  
     draw() {
       d.drawImage(this.picture, this.placement.x, this.placement.y, this.width, this.height);
     }
-    //this function makes spartans to move
+  
     update() {
       if (this.picture) {
         this.draw();
         this.placement.x += this.celerity.x;
         this.placement.y += this.celerity.y;
+
         if(this.placement.x + this.width >= kanhtml.width || this.placement.x  <= 0 ){
             this.celerity.x = -this.celerity.x;
         }
       }
     }
   }
+// Creating couds of invaders everywhere
 
 
-
-//Define arrays for the game
+//constsant variabes of the gameaaa
 const archer = new Archer ()
 const arrows = []
+//const invader1 = new Spartan(0, 0); // (0, 0) placement
+
+// here I work on grid
 let spartanArray = []
-// Adding spartans to spartanArray
+
 for (let x = 1; x < 6; x++){
     for (let y = 0; y < 2; y++){
         spartanArray.push(
@@ -116,10 +126,14 @@ for (let x = 1; x < 6; x++){
                 y
             )
         )
+        //console.log(spartanArray)
     }
 }
   
-//Set default value for event listeners
+// console.log(spartanArray)
+
+
+//key isteners
 const keycodes = {
     a: {
         pressed: false
@@ -129,18 +143,27 @@ const keycodes = {
     },
     space: {
         pressed: false
-    }  
+    }
 }
-// Variable which we use to delete instructions from the list when the player preses the first key
+// variabe to disapear text
 let spacepress = 0
-//Main function which plays the game.
+
+
+
 function animate() {
     requestAnimationFrame(animate)
-    // Add color to canvas
     d.fillStyle = 'black'
     d.fillRect(0, 0, kanhtml.width, kanhtml.height)
+    // Game manual text
     
-    // Write game manual on the canvas
+    //console.log('before spacepressed is 0')
+    // if(keycodes.space.pressed){
+    //    console.log('space for text pressed')
+    //     spacepress = spacepress +1
+    //   console.log(spacepress)
+    // }
+
+
     if(spacepress === 0){
     //text in the center of the screen: 
     d.font = '30px Arial';
@@ -149,9 +172,15 @@ function animate() {
     d.fillText('To play a game use "A" and "D" to move and "S" to fire', 
     kanhtml.width/2, kanhtml.height/2);
     } 
-    //animation of archer
+    //else {
+    //    console.log('nothing to see here')
+    //}
+
+
+
+    
     archer.update()
-    // animation of arrows
+    // animation of projecties
     arrows.forEach((projectile, index) => {
         if(projectile.placement.y + projectile.radius <=0){
             arrows.splice(index, 1)
@@ -159,17 +188,46 @@ function animate() {
             projectile.update()
         }
     })
-     //Move the player and fire when player press keys or buttons
+    //payer veocity
+    // if(keycodes.a.pressed && archer.placement.x >= 0){
+    //    console.log('a option in animate')
+    //    archer.celerity.x = -5
+   // } else if (keycodes.d.pressed && archer.placement.x + archer.width <= kanhtml.width ){
+    //    console.log('d option in animate')
+    //    archer.celerity.x = 5
+    //} else {
+    //    console.log('a or d received but there is no move')
+    //    archer.celerity.x = 0
+    //}
+
+    //if(keycodes.a.pressed){
+    //    console.log('keycodes.a.pressed is true')
+    //}
+
+    //console.log(keycodes.a.pressed)
+
+    //if(keycodes.a.pressed && archer.placement.x >= 0){
+    //    console.log('keycodes.a.pressed && archer.placement.x >= 0 is true')
+    //}
+
+     //payer veocity
      if(keycodes.a.pressed && archer.placement.x >= 0){
+        console.log('a option in animate')
         archer.celerity.x = -5
     } else if (keycodes.d.pressed && archer.placement.x + archer.width <= kanhtml.width ){
+        console.log('d option in animate')
         archer.celerity.x = 5
     } else {
+        //console.log('a or d received but there is no move')
         archer.celerity.x = 0
     }
+
+
+
+
    //try to type objects in the array
    spartanArray.forEach((invad, i) => { invad.update();
-    //deleting arror and spartan when they meet each other.
+    
     arrows.forEach((projectile,j) => {
         if(
             projectile.placement.y - projectile.radius <=
@@ -187,15 +245,23 @@ function animate() {
                   const projectileFound = arrows.find(
                     (projectile2) => projectile2 === projectile
                   )
+      
+
                 spartanArray.splice(i, j)
                 arrows.splice(i, j)
+                console.log(spartanArray.length)
             }, 0 )
         }
     })
+
+
     }
    )
-   // Inform the user that they have won and explain how to start the game again.
+   //console.log('out of the oop enth of spartanArray')
+   //console.log(spartanArray.length)
    if(spartanArray.length === 0){
+        //console.log('congrats you won')
+        //text which informs user that he won and how to pay again 
         d.font = '30px Arial';
         d.fillStyle = 'white';
         d.textAlign = 'center';
@@ -203,34 +269,68 @@ function animate() {
         kanhtml.width/2, kanhtml.height/2);
    }
 }
-//Call the function sto start the game
+
 animate()
 
+
+// Trying to activate browser keycodes
+//function sendKey(key) {
+    //console.log('send key triggered succesfuy and pressed key is : ')
+    //console.log(key)
+//    console.log("html listener " + key);
+//    var kanhtml = document.getElementById("myCanvas");
+    //console.log(kanhtml)
+// kanhtml.focus();
+//    var event = new KeyboardEvent("keydown", {"key": key});
+  //   kanhtml.dispatchEvent(event);
+ //   console.log("event variable is :"+ event)
+ //}
+
  // Send buttons events to KeyboardEvent
+
  function sendKey(key) {
+    console.log("sendKey function received a key for key down");
     let event = new KeyboardEvent('keydown', { key });
     window.dispatchEvent(event);
   }
-
+  
   function unSendKey(key) {
+    console.log("unSend function received the key for key up");
     let event1 = new KeyboardEvent('keyup', { key });
     setTimeout(function(){window.dispatchEvent(event1);}, 100);
   }
   
-//event listener when we press the buttons
+
+
+
+
+//event istener when we press the buttons
 window.addEventListener('keydown', ({ key }) => {
+    
+    console.log("Js listener " + key);
     switch (key) {
         case 'a':
-            spacepress = spacepress +1 
-            keycodes.a.pressed = true 
+            
+            console.log('left down')
+            spacepress = spacepress +1
+            // console.log('keydown keycodes.a.pressed before' +  keycodes.a.pressed)     
+            keycodes.a.pressed = true
+           // console.log('keydown keycodes.a.pressed after' +  keycodes.a.pressed)  
             break
         case 'd':
+            console.log('right down')
             spacepress = spacepress +1
             keycodes.d.pressed = true
             break
         case 's':
+            console.log('space')
             spacepress = spacepress +1
-            //project move
+            //console.log('the vaue of spacetress is ')
+            //console.log(spacepress)
+
+
+
+            //projecti work
             arrows.push(new Arrow({
                 placement: {
                     x:archer.placement.x + archer.width/2,
@@ -242,21 +342,35 @@ window.addEventListener('keydown', ({ key }) => {
                 }
             }
             ))
+            //console.log(arrows)
             break
     }
 })
 
-// event listener when we unpress the button
+
+
+
+// event istener when we unpress the button
+
 //buttons on the screen
 window.addEventListener('keyup', ({ key }) => {
+    console.log('keyup event listener received key from unsendKey Function')
     switch (key) {
-        case 'a':    
+        case 'a':
+            
+       
+            console.log('left up')
+            console.log('keyup keycodes.a.pressed before' +  keycodes.a.pressed)  
             keycodes.a.pressed = false
+            console.log('keyup keycodes.a.pressed before' +  keycodes.a.pressed)  
             break
         case 'd':
+            console.log('right up')
             keycodes.d.pressed = false
             break
         case 's':
+            console.log('space up')
+            
             break
     }
 })
